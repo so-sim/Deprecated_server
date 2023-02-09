1829c9383689968dffad21bdbb715734055a288c
jean202 <hb13pud731@gmail.com>
2023-02-09 12:22:01 +0900

    [feature/jinha-kim/create-custom-constants] fix: modify access modifier to public

--- src/main/java/com/sosim/server/jwt/util/constant/CustomConstant.java
+++ src/main/java/com/sosim/server/jwt/util/constant/CustomConstant.java
@@ -2,9 +2,9 @@ package com.sosim.server.jwt.util.constant;
 
 public class CustomConstant {
 
-    private static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
-    private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
-    private static final String PROVIDER_ID_CLAIM = "providerId";
-    private static final String BEARER = "Bearer ";
+    public static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
+    public static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
+    public static final String PROVIDER_ID_CLAIM = "providerId";
+    public static final String BEARER = "Bearer ";
 
 }
