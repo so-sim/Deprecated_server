0c23973eec3e1f45138b6c77e0e316edaa5ebc4c
jean202 <hb13pud731@gmail.com>
2023-02-20 12:08:49 +0900

    chore : remove some comments on JwtServiceImpl and add some comments on JwtRepository

--- src/main/java/com/sosim/server/jwt/JwtRepository.java
+++ src/main/java/com/sosim/server/jwt/JwtRepository.java
@@ -4,7 +4,7 @@ import com.sosim.server.jwt.dao.JwtDao;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
-// TODO redis repository
+// TODO 현재 쓰이지 않고 있음, 나중에 결국 안쓰게되면 지울것
 public interface JwtRepository extends CrudRepository<JwtDao, String> {
 
     Optional<JwtDao> findByRefreshToken(String refreshToken);
