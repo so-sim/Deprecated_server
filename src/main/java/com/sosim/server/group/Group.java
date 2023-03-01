bf48b0c97024d7a3640caa998944cf1738fcbcce
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-01 15:46:17 +0900

    feat : Group domain 수정

--- src/main/java/com/sosim/server/group/Group.java
+++ src/main/java/com/sosim/server/group/Group.java
@@ -1,10 +1,7 @@
 package com.sosim.server.group;
 
 import com.sosim.server.participant.Participant;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
@@ -12,18 +9,19 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Builder
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Group {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "GROUP_ID")
     private Long id;
 
-    @Column(name = "NAME")
-    private String name;
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "ADMIN_ID")
+    private Long adminId;
 
     @Column(name = "CREATE_DATE")
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
@@ -44,4 +42,25 @@ public class Group {
     @Column(name = "GROUP_TYPE")
     @Enumerated(EnumType.STRING)
     private GroupType groupType;
+
+    @OneToMany(mappedBy = "group")
+    private List<Participant> participantList;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Group(Long adminId, String title, String coverColorType, String groupType) {
+        this.adminId = adminId;
+        this.title = title;
+        this.coverColorType = CoverColorType.valueOf(coverColorType);
+        this.groupType = GroupType.of(groupType);
+        createDate = LocalDateTime.now();
+        updateDate = LocalDateTime.now();
+    }
+
+    public static Group createGroup(Long adminId, String title, String groupType, String coverColorType) {
+        return Group.builder()
+                .adminId(adminId)
+                .title(title)
+                .groupType(groupType)
+                .coverColorType(coverColorType).build();
+    }
 }
--- src/main/java/com/sosim/server/group/GroupType.java
+++ src/main/java/com/sosim/server/group/GroupType.java
@@ -1,4 +1,35 @@
 package com.sosim.server.group;
 
+import lombok.Getter;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Getter
 public enum GroupType {
+    STUDY( "스터디"),
+    CAMPUS("학교, 교내/외 모임"),
+    COMPANY("회사, 사내 모임"),
+    HOBBY("취미, 동호회"),
+    SOCIAL("친구, 사모임"),
+    PROJECT("프로젝트"),
+    ETC("기타");
+
+    private final String label;
+
+    GroupType(String label) {
+        this.label = label;
+    }
+
+    private static final Map<String, GroupType> map =
+        Collections.unmodifiableMap(Stream.of(values())
+            .collect(Collectors.toMap(GroupType::getLabel, Function.identity())));
+
+    public static GroupType of(String label) {
+        return map.get(label);
+    }
 }
