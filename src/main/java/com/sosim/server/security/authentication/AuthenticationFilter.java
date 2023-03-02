ca823b7115699b4f65d22882b78eba0556e551a9
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-02 23:08:01 +0900

    feat : AuthenticationFilter JWT 인증 추가

--- src/main/java/com/sosim/server/security/authentication/AuthenticationFilter.java
+++ src/main/java/com/sosim/server/security/authentication/AuthenticationFilter.java
@@ -9,44 +9,76 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sosim.server.security.AuthUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j
 @RequiredArgsConstructor
 public class AuthenticationFilter extends OncePerRequestFilter {
 
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+
     private final JwtProvider jwtProvider;
     private final JwtService jwtService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // "/login"으로 들어오는 요청은 Filter 작동 X
-        if (request.getRequestURI().equals(NO_CHECK_URL)) {
-            filterChain.doFilter(request, response); // "/login" 요청이 들어오면, 다음 필터 호출
-            return; // return으로 이후 현재 필터 진행 막기 (안해주면 아래로 내려가서 계속 필터 진행시킴)
-        }
+//        //TODO 이부분도 따로 메서드로 뺄지?
+//        String refreshToken = jwtProvider.extractRefreshToken(request)
+//            .filter(jwtProvider::isTokenValid)
+//            .orElse(null);
+//
+//        // 요청헤더에 RefreshToken이 존재했다면, AccessToken이 만료된것
+//        // RefreshToken을 redis의 것과 비교, 일치시 AccessToken 재발급
+//        if (refreshToken != null) {
+//            jwtService.verifyRefreshTokenAndReIssueAccessToken(request, response, refreshToken);
+//            filterChain.doFilter(request, response);
+//            return; // AccessToken을 재발급하고 인증 처리 방지를 위해 return으로 필터 진행 막기
+//        }
+//
+//        // RefreshToken이 없거나 유효하지 않다면, AccessToken을 검사하고 인증을 처리하는 로직 수행
+//        // AccessToken이 없거나 유효하지 않다면, 인증 객체가 담기지 않은 상태로 다음 필터로 넘어가기 때문에 403 에러 발생
+//        // AccessToken이 유효하다면, 인증 객체가 담긴 상태로 다음 필터로 넘어가기 때문에 인증 성공
+//        if (refreshToken == null) {
+////            checkAccessTokenAndAuthentication(request, response, filterChain);
+//        }
+//
+//        filterChain.doFilter(request, response);
 
-        //TODO 이부분도 따로 메서드로 뺄지?
-        String refreshToken = jwtProvider.extractRefreshToken(request)
-            .filter(jwtProvider::isTokenValid)
-            .orElse(null);
+        // 사용자의 요청에 존재하는 Authorization Header Value
+        // 1. 엑세스 토큰 들고 온 유저들만 검사
+        // 2. 리프레쉬 토큰은 검사 필요 X
+        String authorizationHeader = request.getHeader(AUTHORIZATION_HEADER);
 
-        // 요청헤더에 RefreshToken이 존재했다면, AccessToken이 만료된것
-        // RefreshToken을 redis의 것과 비교, 일치시 AccessToken 재발급
-        if (refreshToken != null) {
-            jwtService.verifyRefreshTokenAndReIssueAccessToken(request, response, refreshToken);
+        if(authorizationHeader == null){
             filterChain.doFilter(request, response);
-            return; // AccessToken을 재발급하고 인증 처리 방지를 위해 return으로 필터 진행 막기
+            return;
         }
 
-        // RefreshToken이 없거나 유효하지 않다면, AccessToken을 검사하고 인증을 처리하는 로직 수행
-        // AccessToken이 없거나 유효하지 않다면, 인증 객체가 담기지 않은 상태로 다음 필터로 넘어가기 때문에 403 에러 발생
-        // AccessToken이 유효하다면, 인증 객체가 담긴 상태로 다음 필터로 넘어가기 때문에 인증 성공
-        if (refreshToken == null) {
-//            checkAccessTokenAndAuthentication(request, response, filterChain);
-        }
+        String token = getToken(authorizationHeader);
+
+        setAuthToSecurityContextHolder(token);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(String authorizationHeader){
+        return authorizationHeader.substring(BEARER_PREFIX.length());
+    }
+
+    private void setAuthToSecurityContextHolder(String token) {
+        // Security Context Holder 에 유저 정보 저장 메서드
+        String userId = jwtProvider.extractId(token);
+        AuthUser context = AuthUser.builder().id(userId).build();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(context, null, context.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
 
