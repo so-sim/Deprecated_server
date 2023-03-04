0b33b0c28012ea789fe585550a31350520c7cbc1
jean202 <hb13pud731@gmail.com>
2023-03-05 05:50:39 +0900

    feat : use getUser in UserController

--- src/main/java/com/sosim/server/jwt/util/constant/MessageConstant.java
+++ src/main/java/com/sosim/server/jwt/util/constant/MessageConstant.java
@@ -2,4 +2,5 @@ package com.sosim.server.jwt.util.constant;
 
 public class MessageConstant {
     public static final String REISSUETOKEN = "AccessToken과 RefreshToken이 성공적으로 재발급 되었습니다.";
+    public static final String USER_INFO_SUCCESS = "회원 정보가 성공적으로 조회되었습니다.";
 }
