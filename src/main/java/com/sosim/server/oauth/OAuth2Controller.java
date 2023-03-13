46e3c35483d5e325521b0e028a425df6dcad56b5
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-13 16:54:36 +0900

    refactoring : GetMapping -> PostMapping 수정

--- src/main/java/com/sosim/server/oauth/OAuth2Controller.java
+++ src/main/java/com/sosim/server/oauth/OAuth2Controller.java
@@ -6,7 +6,6 @@ import com.sosim.server.jwt.JwtService;
 import com.sosim.server.oauth.dto.OAuth2JwtResponseDto;
 import com.sosim.server.type.SocialType;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,7 +18,7 @@ public class OAuth2Controller {
     private final OAuth2Service oAuth2Service;
     private final JwtService jwtService;
 
-    @GetMapping("/login/oauth2/code/{socialType}")
+    @PostMapping("/login/oauth2/code/{socialType}")
     public ResponseEntity<?> login(@PathVariable("socialType") String socialType, @RequestParam("code") String code,
                                    HttpServletResponse response) throws JsonProcessingException {
         OAuth2JwtResponseDto tokens = oAuth2Service.login(SocialType.getSocialType(socialType), code);
