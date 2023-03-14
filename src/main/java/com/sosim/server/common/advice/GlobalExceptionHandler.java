8238e1edf273364ab9f8c4011b88e6d7d60ae2c0
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-14 18:14:40 +0900

    refactoring : 예외 처리 수정

--- src/main/java/com/sosim/server/common/advice/GlobalExceptionHandler.java
+++ src/main/java/com/sosim/server/common/advice/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.sosim.server.common.advice;
 
-import com.sosim.server.common.response.ExceptionResponse;
+import com.sosim.server.common.response.Response;
 import com.sosim.server.config.exception.CustomException;
+import com.sosim.server.type.CodeType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +14,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = CustomException.class)
     protected ResponseEntity<?> handleCustomException(CustomException exception) {
-        return new ResponseEntity<>(ExceptionResponse.create(exception), exception.getHttpStatus());
+        return new ResponseEntity<>(Response.create(exception.getCodeType(), exception.getContent()),
+                exception.getCodeType().getHttpStatus());
+    }
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    protected ResponseEntity<?> handleCustomException(HttpMessageNotReadableException exception) {
+        return new ResponseEntity<>(Response.create(CodeType.BINDING_ERROR, null),
+                HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/sosim/server/config/exception/CustomException.java
+++ src/main/java/com/sosim/server/config/exception/CustomException.java
@@ -10,78 +10,23 @@ import org.springframework.http.ResponseEntity;
 @Getter
 public class CustomException extends NestedRuntimeException {
 
-    @Getter
     private CodeType codeType;
-
-    private String field;
-    private String fieldMessage;
-    private String message;
-    private HttpStatus httpStatus;
-
-    private String code;
+    private Content content;
 
     public CustomException(CodeType codeType) {
-        this(codeType.getMessage(), codeType.getHttpStatus(), codeType.getCode());
+        super(codeType.getMessage());
+        this.codeType = codeType;
     }
 
-    public CustomException(String field, String message, CodeType codeType) {
-        this(field, message, codeType.getHttpStatus(), codeType.getCode());
+    public CustomException(CodeType codeType, String field, String message) {
+        this(codeType);
+        content = new Content(field, message);
     }
 
-    public CustomException(String message, CodeType codeType) {
-        this(message, codeType.getHttpStatus(), codeType.getCode());
-    }
-
-    public CustomException(CodeType codeType, String field, String fieldMessage) {
-        this(field, fieldMessage, codeType.getMessage(), codeType.getHttpStatus(), codeType.getCode());
-    }
-
-    public CustomException(String message, HttpStatus httpStatus, int code) {
-        super(message);
-        this.message = message;
-        this.httpStatus = httpStatus;
-        this.code = String.valueOf(code);
-    }
-
-    public CustomException(String message, HttpStatus httpStatus, String code) {
-        super(message);
-        this.message = message;
-        this.httpStatus = httpStatus;
-        this.code = code;
-    }
-
-    public CustomException(String field, String message, HttpStatus httpStatus, String code) {
-        super(message);
-        this.field = field;
-        this.message = message;
-        this.httpStatus = httpStatus;
-        this.code = code;
-    }
-
-    public CustomException(String field, String fieldMessage, String message, HttpStatus httpStatus, String code) {
-        super(message);
-        this.field = field;
-        this.fieldMessage = fieldMessage;
-        this.message = message;
-        this.httpStatus = httpStatus;
-        this.code = code;
-    }
-
-    public RestError toRestError() {
-        return new RestError(this.field, this.code, this.message, this.httpStatus);
-    }
-
-    public ResponseEntity<RestError> toResponseEntity() {
-        return new ResponseEntity<>(this.toRestError(), this.httpStatus);
-    }
-
-    @AllArgsConstructor
     @Getter
-    public static class RestError {
+    @AllArgsConstructor
+    private static class Content {
         private String field;
-        private String code;
         private String message;
-        private HttpStatus status;
     }
-
 }
