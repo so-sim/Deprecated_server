08ab6e8d9463d25235dbba900412655cf03e8568
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-23 19:54:32 +0900

    refactoring : cors addMapping url 수정

--- src/main/java/com/sosim/server/config/WebMvcConfig.java
+++ src/main/java/com/sosim/server/config/WebMvcConfig.java
@@ -10,14 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3090/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders("*")
-                .allowCredentials(false)
-                .maxAge(3600);
-
-        registry.addMapping("/login/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3090/")
                 .allowedMethods("GET", "POST", "PATCH")
                 .allowedHeaders("*")
