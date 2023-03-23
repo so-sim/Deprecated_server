c5ee994f9965b1926285bed6fdd3dd4a61d09356
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-23 19:50:16 +0900

    refactoring : cors 허용 url 재수정

--- src/main/java/com/sosim/server/config/WebMvcConfig.java
+++ src/main/java/com/sosim/server/config/WebMvcConfig.java
@@ -11,14 +11,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3090")
+                .allowedOrigins("http://localhost:3090/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(3600);
 
         registry.addMapping("/login/**")
-                .allowedOrigins("http://localhost:3090")
+                .allowedOrigins("http://localhost:3090/")
                 .allowedMethods("GET", "POST", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
