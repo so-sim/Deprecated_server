f6f0dcacd6aef9f8f1f05a25106db4743d948f69
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-23 20:19:25 +0900

    refactoring : cors 수정

--- src/main/java/com/sosim/server/config/WebMvcConfig.java
+++ src/main/java/com/sosim/server/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
@@ -11,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3090/")
+                .allowedOrigins("http://localhost:3090")
                 .allowedMethods("GET", "POST", "PATCH", "OPTIONS", "DELETE", "PUT")
                 .allowedHeaders("*")
                 .allowCredentials(true)
