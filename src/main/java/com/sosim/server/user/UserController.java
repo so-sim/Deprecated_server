87992ef780bd61e8376f20c463dc9100d688ef72
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-23 17:49:44 +0900

    feat : 서비스 이용 약관 동의 변경 API 구현

--- src/main/java/com/sosim/server/user/UserController.java
+++ src/main/java/com/sosim/server/user/UserController.java
@@ -1,30 +1,32 @@
 package com.sosim.server.user;
 
 import com.sosim.server.common.response.Response;
+import com.sosim.server.security.AuthUser;
 import com.sosim.server.type.CodeType;
 import com.sosim.server.user.dto.req.UserWithdrawalReq;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("api/user")
 public class UserController {
 
     private final UserService userService;
 
-    @PutMapping
+    @PutMapping("/api/user")
     public ResponseEntity<?> withdrawalUser(@Valid @RequestBody UserWithdrawalReq userWithdrawalReq) {
         this.userService.withdrawalUser(userWithdrawalReq);
         return new ResponseEntity<>(Response.create(CodeType.USER_WITHDRAWAL_SUCCESS, null), CodeType.USER_WITHDRAWAL_SUCCESS.getHttpStatus());
+    }
+
+    @PatchMapping("/login/permit")
+    public ResponseEntity<?> permit(@AuthenticationPrincipal AuthUser authUser) {
+        userService.permitUser(Long.valueOf(authUser.getId()));
+        return new ResponseEntity<>(Response.create(CodeType.PERMIT_USER, null), CodeType.PERMIT_USER.getHttpStatus());
     }
 }
