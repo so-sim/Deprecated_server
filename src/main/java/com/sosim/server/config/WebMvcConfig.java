694849a5ed25ca4cf8c0c4faca086aa72413c543
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-23 18:21:34 +0900

    refactoring : WebMvcConfig 어노테이션 추가

--- src/main/java/com/sosim/server/config/WebMvcConfig.java
+++ src/main/java/com/sosim/server/config/WebMvcConfig.java
@@ -1,21 +1,25 @@
 package com.sosim.server.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3090")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(3600);
 
         registry.addMapping("/login/**")
-                .allowedOrigins("http://localhost:3090")
-                .allowedMethods("GET", "POST")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(3600);
