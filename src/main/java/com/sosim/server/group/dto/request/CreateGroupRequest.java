6b8bb811c4a9d3f1b5bd69c3f7160d177f766fd3
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-25 01:22:23 +0900

    refactoring : json key 값 Front 와 맞추기

--- src/main/java/com/sosim/server/group/dto/request/CreateGroupRequest.java
+++ src/main/java/com/sosim/server/group/dto/request/CreateGroupRequest.java
@@ -12,12 +12,12 @@ import javax.validation.constraints.Size;
 public class CreateGroupRequest {
     @NotBlank
     @Size(min = 1, max = 15, message = "모임 이름은 최소 1글자, 최대 15글자까지 허용됩니다.")
-    @JsonProperty("groupName")
+    @JsonProperty("title")
     private String title;
 
     @NotBlank
     @Size(min = 1, max = 15, message = "닉네임은 최소 1글자, 최대 15글자까지 허용됩니다.")
-    @JsonProperty("myName")
+    @JsonProperty("nickname")
     private String nickname;
 
     @JsonProperty("type")
