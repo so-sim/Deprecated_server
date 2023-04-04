9ce2cc5a4ff13d49825774e0c51385d6157f9cc3
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-04-04 17:53:15 +0900

    fix : CORS 에러 Origin 추가

--- src/main/java/com/sosim/server/config/WebMvcConfig.java
+++ src/main/java/com/sosim/server/config/WebMvcConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
@@ -12,14 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3090")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-
-        registry.addMapping("/**")
-                .allowedOrigins("https://sosim-manager.com")
+                .allowedOrigins("http://localhost:3090", "https://sosim-manager.com")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
