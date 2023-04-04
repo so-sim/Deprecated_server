b6b1fc47d6633acb8eb996b149193efb2733ba6d
jean202 <hb13pud731@gmail.com>
2023-04-04 16:03:38 +0900

    fix : changed EventCreateReq validation about grounds

--- src/main/java/com/sosim/server/event/dto/req/EventModifyReq.java
+++ src/main/java/com/sosim/server/event/dto/req/EventModifyReq.java
@@ -7,21 +7,16 @@ import lombok.Setter;
 @Getter
 public class EventModifyReq {
 
-    // 팀원
     private String userName;
 
     @Setter
     private User user;
 
-    // 사유 발생 날짜
     private String groundsDate;
 
-    // 금액
     private Long payment;
 
-    // 사유
     private String grounds;
 
-    //  납부여부
     private String paymentType;
 }
