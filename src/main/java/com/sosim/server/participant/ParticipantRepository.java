e56882d86430c861e8109791ade581d631fb691c
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-04-11 14:44:07 +0900

    fix : 모임 참가자들 조회 조건 수정

--- src/main/java/com/sosim/server/participant/ParticipantRepository.java
+++ src/main/java/com/sosim/server/participant/ParticipantRepository.java
@@ -23,6 +23,6 @@ public interface ParticipantRepository extends JpaRepository<Participant, Long> 
 
     Slice<Participant> findByIdLessThanAndStatusTypeAndUserIdOrderByIdDesc(Long participantId, StatusType statusType, Long userId, Pageable pageable);
 
-    @Query("select p.user.id from Participant p where p.group.id = :groupId and p.nickname in (:nickname)")
+    @Query("select p.user.id from Participant p where p.group.id = :groupId and p.statusType = 'ACTIVE' and p.nickname in (:nickname)")
     List<Long> findByGroupIdAndNicknameIn(@Param("groupId") Long groupId, @Param("nickname") List<String> nickname);
 }
