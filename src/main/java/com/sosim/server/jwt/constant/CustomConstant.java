2e428f9c96c405583939b5993a09e97dd45a5797
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-04-17 19:06:31 +0900

    etc : 불필요 변수 삭제

--- src/main/java/com/sosim/server/jwt/constant/CustomConstant.java
+++ src/main/java/com/sosim/server/jwt/constant/CustomConstant.java
@@ -1,16 +1,8 @@
 package com.sosim.server.jwt.constant;
 
 public class CustomConstant {
-
-    public static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
-    public static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
     public static final String SET_COOKIE = "Set-Cookie";
-
     public static final String NONE = "None";
     public static final String ID = "id";
-    public static final String SOCIAL_TYPE = "socialType";
-    public static final String SOCIAL_ID = "socialId";
-    public static final String BEARER = "Bearer ";
     public static final String REFRESH_TOKEN = "refreshToken";
-
 }
